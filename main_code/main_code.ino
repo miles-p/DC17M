4d5568994829a3f0654f2d333749e1ce4cee10a3
Miles Punch <miles.anyprintz@gmail.com>
2024-01-04 07:13:55 +1100

    github pushed wrong :angry:

--- main_code/main_code.ino
+++ main_code/main_code.ino
@@ -1,31 +1,35 @@
+const int switchPin = 4;             // Pin connected to the switch
+const int ledPin = 6;                // Pin connected to the LED
+int switchState = 0;                 // Variable to store the current state of the switch
+int lastSwitchState = 0;             // Variable to store the previous state of the switch
+unsigned long lastDebounceTime = 0;  // Last time the switch state changed
+unsigned long debounceDelay = 50;    // Debounce time in milliseconds
+bool isHolding;
+unsigned long timeLastPressed;  // needs to be long otherwise the millis function overloads and funky shit starts happening (this happens as the time hits the 16 bit int def. When it goes to long, this limit is raised to 32 bit)
+int holdTime = 200;
+int bulletRefresh = 301;
+int bulletCount;
+int fireSpeed = 40;
+char dig1;
+char dig2;
+char dig3;
+
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include "Adafruit_LEDBackpack.h"
 
 Adafruit_AlphaNum4 alpha4 = Adafruit_AlphaNum4();
 
-const int switchPin = 4;  // Pin connected to the switch
-const int ledPin = 6;     // Pin connected to the LED
-
-int switchState = 0;      // Variable to store the current state of the switch
-int lastSwitchState = 0;  // Variable to store the previous state of the switch
-unsigned long lastDebounceTime = 0;  // Last time the switch state changed
-unsigned long debounceDelay = 50;    // Debounce time in milliseconds
-bool isHolding;
-unsigned long timeLastPressed; // needs to be long otherwise the millis function overloads and funky shit starts happening (this happens as the time hits the 16 bit int def. When it goes to long, this limit is raised to 32 bit)
-int holdTime = 200;
-int bulletRefresh = 300;
-int bulletCount;
-int fireSpeed = 40;
-char counterArray[3];
-
 void setup() {
-  alpha4.begin(0x70);
+  alpha4.begin(0x70);  // pass in the address
   pinMode(switchPin, INPUT_PULLUP);  // Use internal pull-up resistor
   pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
   bulletCount = bulletRefresh;
-  alpha4.clear();
+  alpha4.writeDigitAscii(0,'0');
+  alpha4.writeDigitAscii(1,'3');
+  alpha4.writeDigitAscii(2,'0');
+  alpha4.writeDigitAscii(3,'0');
   alpha4.writeDisplay();
 }
 
@@ -43,8 +47,17 @@ void constantShot() {
   digitalWrite(ledPin, LOW);
   delay(fireSpeed);
 }
+int splitDigits(int input) {
+  //Serial.println(bulletCount / 100); // 1nd Digit
+  //Serial.println((bulletCount / 10) % 10); // 2nd Digit
+  //Serial.println(bulletCount % 10); // 3rd Digit
+  dig1 = '0' + (input / 100);
+  dig2 = '0' + ((input / 10) % 10);
+  dig3 = '0' + (input % 10);
+}
 
 void loop() {
+  alpha4.clear();
   isHolding = false;
   int reading = digitalRead(switchPin);
 
@@ -54,7 +67,7 @@ void loop() {
     if (reading == LOW) {
       timeLastPressed = millis();
     }
-      //Serial.println("Finally high "+millis());
+    //Serial.println("Finally high "+millis());
     if (reading == HIGH) {
       isHolding = false;
     }
@@ -72,11 +85,17 @@ void loop() {
       }
     }
   }
-  if (lastSwitchState == LOW && reading == LOW && millis()-timeLastPressed > holdTime && bulletCount > 0) {
+  if (lastSwitchState == LOW && reading == LOW && millis() - timeLastPressed > holdTime && bulletCount > 0) {
     isHolding = true;
     constantShot();
   }
-  Serial.println(bulletCount);
-  // Save the current switch state for comparison in the next iteration
+  splitDigits(bulletCount);
+  Serial.println(dig1);
+  Serial.println(dig2);
+  Serial.println(dig3);
+  alpha4.writeDigitAscii(1,dig1);
+  alpha4.writeDigitAscii(2,dig2);
+  alpha4.writeDigitAscii(3,dig3); 
   lastSwitchState = reading;
+  alpha4.writeDisplay();
 }
